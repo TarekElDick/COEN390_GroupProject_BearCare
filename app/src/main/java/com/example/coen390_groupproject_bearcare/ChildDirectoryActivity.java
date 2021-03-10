304ac4b1530752f14507134dd8e99f3c5d3d2bb7
TarekElDick <tarekdeek17@gmail.com>
2021-03-10 23:12:32 +0300

    #1 #2 #14 #25 #26 Create main user page , dynamicly updating display name , added a insertChildDialog and fragment, currently working on adding child to database. Also added icons to give users.

--- app/src/main/java/com/example/coen390_groupproject_bearcare/ChildDirectoryActivity.java
+++ app/src/main/java/com/example/coen390_groupproject_bearcare/ChildDirectoryActivity.java
@@ -1,14 +1,26 @@
 package com.example.coen390_groupproject_bearcare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.coen390_groupproject_bearcare.Model.Child;
 import com.example.coen390_groupproject_bearcare.Model.Date;
+import com.example.coen390_groupproject_bearcare.fragments.InsertChildDialog;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,28 +32,35 @@ public class ChildDirectoryActivity extends AppCompatActivity {
     private RecyclerView.LayoutManager layoutManager;
     List<Child> children = new ArrayList<>();
 
+    // Our floating action button class object
+    protected FloatingActionButton addChildButton;
+
+    String TAG = "debug_childDirectory";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child_directory);
+
+        // Because we are not adding our own toolbar it is sufficient enough to
+        // get the support of our own actionBar, and set the UP button enabled.
+        // Connections done in the manifest file.
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        setUpUI();
 
         // TODO replace this code where we fill the list of children
         // these should actually come from our DB of children
         children.add(new Child(0,0,0, "Ella Fitzgerald", new Date(1, 1, 2015)));
         children.add(new Child(0,0,0, "Nina Simone", new Date(1, 1, 2016)));
         children.add(new Child(0,0,0, "Charles Mingus", new Date(1, 1, 2015)));
-        children.add(new Child(0,0,0, "Ella Fitzgerald", new Date(1, 1, 2015)));
-        children.add(new Child(0,0,0, "Nina Simone", new Date(1, 1, 2016)));
-        children.add(new Child(0,0,0, "Charles Mingus", new Date(1, 1, 2015)));
-        children.add(new Child(0,0,0, "Ella Fitzgerald", new Date(1, 1, 2015)));
-        children.add(new Child(0,0,0, "Nina Simone", new Date(1, 1, 2016)));
-        children.add(new Child(0,0,0, "Charles Mingus", new Date(1, 1, 2015)));
-        children.add(new Child(0,0,0, "Ella Fitzgerald", new Date(1, 1, 2015)));
-        children.add(new Child(0,0,0, "Nina Simone", new Date(1, 1, 2016)));
-        children.add(new Child(0,0,0, "Charles Mingus", new Date(1, 1, 2015)));
-        children.add(new Child(0,0,0, "Ella Fitzgerald", new Date(1, 1, 2015)));
-        children.add(new Child(0,0,0, "Nina Simone", new Date(1, 1, 2016)));
-        children.add(new Child(0,0,0, "Charles Mingus", new Date(1, 1, 2015)));
+
+
+
+
+    }
+
+
+    public void setUpUI(){
 
         // all recycler view setup
         recyclerView = findViewById(R.id. recyclerView_childList);
@@ -50,5 +69,65 @@ public class ChildDirectoryActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
         adapter = new ChildRecyclerViewAdapter(children, this);
         recyclerView.setAdapter(adapter);
+
+        // Connections
+        addChildButton = findViewById(R.id.floatingActionButtonAddChild_childDirectory);
+
+        // Onclick listener for our add child button
+        addChildButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Log.d(TAG, "User accessing insertChildDialog");
+
+                // Now we want to open a dialog fragment, requires two things
+                // The layout which defines what we will see
+                // and A class for it that expands from dialogFragment.
+                InsertChildDialog dialog = new InsertChildDialog();
+
+                // Now we want to show the fragment
+                dialog.show(getSupportFragmentManager(),"Insert Child");
+
+            }
+        });
+
+        // end of setUpUI
+    }
+
+
+
+    // Created our menu layout file in the resource directory (res/menu/menu_DASHBOARD), and we connected it to this activity.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_dashboard, menu);
+        return true;
+    }
+
+    // Now we need to specify what happens once our menu items are clicked.
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuItem_settings:
+
+                Log.d(TAG, "User settings clicked");
+                // TO-DO settings activity for language and preferences.
+
+                return true;
+
+            case R.id.menuItem_logout:
+
+                Log.d(TAG, "User is logging out");
+                Toast.makeText(this, "Logging out", Toast.LENGTH_SHORT).show();
+
+                // We sign out the firebase user and they are sent to the login activity (MainActivity.java)
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class ));
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/example/coen390_groupproject_bearcare/UserMainPageActivity.java
+++ app/src/main/java/com/example/coen390_groupproject_bearcare/UserMainPageActivity.java
@@ -1,32 +1,132 @@
 package com.example.coen390_groupproject_bearcare;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class UserMainPageActivity extends AppCompatActivity {
 
-    private Button buttonLogout;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
+
+    private TextView displayName, accessChildDirectory;
+
+
+    String TAG = "debug_dashboard";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_main_page);
 
-        buttonLogout = findViewById(R.id.buttonLogout_dashboard);
 
-        buttonLogout.setOnClickListener(new View.OnClickListener() {
+        // SetUpUI function
+        setUpUI();
+
+        // Initializing Firebase Authentication.
+        mAuth = FirebaseAuth.getInstance();
+
+        // Initializing the user
+        user = mAuth.getCurrentUser();
+
+
+
+
+
+
+        // end of onCreate()
+    }
+
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (user != null){
+            // user is signed in
+            Log.d(TAG, "User is signed in");
+            displayName.setText(user.getDisplayName());
+
+            // TO_D0 get the users information from fireStore
+
+
+            // 1) get children of users if parent
+            // 2) get temperature history.
+
+        } else {
+            Log.d(TAG, "User is signed out");
+            startActivity(new Intent(getApplicationContext(),MainActivity.class ));
+        }
+    }
+
+    public void setUpUI() {
+
+        // Connections
+        displayName = findViewById(R.id.textViewUserName_dashboard);
+        accessChildDirectory = findViewById(R.id.textViewAccessChildDirectory_dashboard);
+
+        // onClickListeners
+        accessChildDirectory.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                FirebaseAuth.getInstance().signOut();
-
-                startActivity(new Intent(getApplicationContext(),MainActivity.class ));
+                Log.d(TAG, "User accessing child directory");
+                startActivity(new Intent(getApplicationContext(),ChildDirectoryActivity.class ));
             }
         });
 
+
+        //end of setUpUI function
     }
+
+    // Created our menu layout file in the resource directory (res/menu/menu_DASHBOARD), and we connected it to this activity.
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_dashboard, menu);
+        return true;
+    }
+
+    // Now we need to specify what happens once our menu items are clicked.
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menuItem_settings:
+
+                // TO-DO settings activity for language and preferences.
+
+                return true;
+
+            case R.id.menuItem_logout:
+
+                Log.d(TAG, "User is logging out");
+                Toast.makeText(this, "Logging out", Toast.LENGTH_SHORT).show();
+
+                // We sign out the firebase user and they are sent to the login activity (MainActivity.java)
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(),MainActivity.class ));
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+
+    // end of activity
 }
