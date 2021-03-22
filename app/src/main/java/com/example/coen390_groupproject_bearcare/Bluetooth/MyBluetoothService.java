6bb197dfda34297df118474c6f2041bfb7aaf962
rhans <rhans92@hotmail.com>
2021-03-22 17:37:32 -0400

    #3.14 #3.10 #3.9
    Filtered paired bluetooth list.
    Pair to device via list.
    Added discovery function to BluetoothScanner.
    Added location services request to BluetoothScanner.
    Added functionality to turn on location services if they are off.
    Added receivers for increased coding flexibility.
    Discovered list filtering currently causes errors - when filtering a null pointer exception is thrown. Or if filtering is applied the device has the wrong address.
    A function needs to be created to pair - this will be done this sprint.
    Currently, pairing causes the list of disovered devcices to load twice.

--- app/src/main/java/com/example/coen390_groupproject_bearcare/Bluetooth/MyBluetoothService.java
+++ app/src/main/java/com/example/coen390_groupproject_bearcare/Bluetooth/MyBluetoothService.java
@@ -4,7 +4,9 @@ package com.example.coen390_groupproject_bearcare.Bluetooth;
 import android.annotation.SuppressLint;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.util.Log;
 import android.widget.Toast;
 import java.io.IOException;
@@ -14,7 +16,19 @@ import java.util.UUID;
 public class MyBluetoothService {
     private final String TAG = "MyBluetoothService";
     private static BluetoothSocket btSocket;
-    private static BluetoothAdapter BtAdapter;      // bluetooth adapter object
+    private static BluetoothAdapter BtAdapter;                      // bluetooth adapter object
+    Context btServiceContext;
+    private AcceptThread insecureAcceptThread;
+    private static final String appName = "OurApp";
+    private static final UUID  myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");        // just moved @ MC's code
+    private ConnectThread connectedThread;
+    private BluetoothDevice serviceDevice;
+    private UUID deviceUUID;
+
+    public MyBluetoothService(Context context) {                    // constructor
+        btServiceContext = context;
+        BtAdapter = BluetoothAdapter.getDefaultAdapter();           // get default adapter on initialization
+    }
 
     @SuppressLint("HardwareIds")
     public void connectBluetoothDevice(String macAddress) throws IOException
@@ -25,7 +39,7 @@ public class MyBluetoothService {
         BluetoothAdapter myBluetooth;
         String address = macAddress;
         String name = "a";
-        UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
 
         myBluetooth = BluetoothAdapter.getDefaultAdapter(); //get the mobile bluetooth device
         BluetoothDevice device = myBluetooth.getRemoteDevice(address); //connects to the device's address and checks if it's available
@@ -73,5 +87,47 @@ public class MyBluetoothService {
         return temperatureReading;
     }
 
+    private class AcceptThread extends Thread{
+        private final BluetoothServerSocket btServerSocket;
 
+        public AcceptThread(){
+            BluetoothServerSocket temp = null;
+
+            // create a new listening server socket
+
+            try {
+                temp = BtAdapter.listenUsingRfcommWithServiceRecord(appName, myUUID);
+            }catch (IOException e){
+
+            }
+
+            btServerSocket = temp;
+        }
+
+        public void run(){
+            try {
+                BluetoothSocket socket = null;
+
+                // this wil only return on successful connection or exceptions
+
+                socket = btServerSocket.accept();
+            }catch(IOException e){
+                Log.e(TAG, "run: " + e.getMessage() );
+            }
+        }
+
+        public void cancel(){
+            try {
+                btServerSocket.close();
+            }catch (IOException e)
+            {
+                Log.e(TAG, "cancel: " + e.getMessage() );
+            }
+        }
+
+    }
+
+    private class ConnectThread extends Thread{
+        
+    }
 }
